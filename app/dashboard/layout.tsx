a564140d23d3f88ee9ae786fb0b0c3c7307a3348
muzazr <zakiazhar046@gmail.com>
2026-01-29 22:24:31 +0700

    make modal optional

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ export default function DashboardLayout ({
     children,
 }: {
     children:  React.ReactNode
-    modal: React.ReactNode
+    modal?: React.ReactNode
 }) {
     return (
         <div className="min-h-screen bg-[#151515] px-4 lg:px-8 py-4 lg:py-8">
